b30d1cd095fdba6323ff532908cfd6619a893434
HM245 <87131178+HM245@users.noreply.github.com>
2024-08-24 14:41:01 +0530

    Delete player.js

--- player.js
+++
@@ -1,7 +0,0 @@
-console.log("Aao saalo");
-let audioElement = new Audio('media/songs/You and Me_64(PagalWorld.com.sb).mp3');
-function playsong() {
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-    }
-}
